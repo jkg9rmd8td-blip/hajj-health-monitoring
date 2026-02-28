a8aa510a9f3dbdef938fa9bcb7b9bf8b60e7b069
abdulaziz <jkg9rmd8td@privaterelay.appleid.com>
2026-02-28 12:24:52 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="ar" dir="rtl">
       <head>
         <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@400;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@400;600;700;800;900&display=swap"
           rel="stylesheet"
         />
       </head>
